5e79858a1e3cb7baf150962162955e712f0c0272
caioh123 <caionwebdev@gmail.com>
2021-09-02 11:31:06 -0300

    feature: added clients page and dashboard changing page on click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
-import Pedidos from './pages/pedidos';
-import Home from './pages/home';
+import "./App.css";
+import Pedidos from "./pages/pedidos";
+import Home from "./pages/home";
+import { Context } from "./context/sectionContext";
+import ClientsPage from "./pages/ClientsPage.js";
+import { useSection } from "./context/sectionContext";
 
 function App() {
-  return (
-    <Home/>
-  );
+  const { section } = useSection();
+
+  const renderDashboard = (section) => {
+    if (section === "Pedidos") {
+      return <Pedidos />;
+    }
+    if (section === "Clientes") {
+      return <ClientsPage />;
+    }
+    return <Home />;
+  };
+
+  return renderDashboard(section);
 }
 
 export default App;
