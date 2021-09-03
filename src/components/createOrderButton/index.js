0eeae9e7a20c7a4f5d94359d39024cce2e6aa141
emersonteles <emersonteles21@gmail.com>
2021-09-03 16:50:12 -0300

    Creating Order Section

--- src/components/createOrderButton/index.js
+++ src/components/createOrderButton/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import './styles.css';
 import { PlusIcon } from '@heroicons/react/outline';
 
-export default function CreateOrderButton() {
+export default function CreateOrderButton(props) {
   return (
-    <button type="button" className="new-order-button">
+    <button type="button" className="new-order-button" onClick={props.onClick}>
       <PlusIcon className="plus-icon" />
       Novo Pedido
     </button>
--- src/pages/pedidos/index.js
+++ src/pages/pedidos/index.js
@@ -1,12 +1,18 @@
 /* eslint-disable react/style-prop-object */
 /* eslint-disable no-unused-vars */
+import { useState } from 'react';
 import CreateOrderButton from '../../components/createOrderButton';
+import CreateOrderSection from '../../components/createOrderSection';
 import FilterButton from '../../components/filterButton';
 import OrdersList from '../../components/orders';
 import SearchInput from '../../components/searchInput';
 import Sidebar from '../../components/sidebar';
 
 export default function Pedidos() {
+  const [open, setOpen] = useState(false);
+  function toggleModal() {
+    setOpen(!open);
+  }
   return (
     <Sidebar>
       <div className="py-6">
@@ -16,11 +22,16 @@ export default function Pedidos() {
         </div>
         <div className="flex flex-wrap items-center mt-5 mb-5 px-4 sm:px-6 md:px-8 gap-4">
           <SearchInput />
-          <FilterButton>Filtro</FilterButton>
-          <CreateOrderButton />
+          <FilterButton />
+          <CreateOrderButton onClick={() => toggleModal()} />
         </div>
         <OrdersList />
       </div>
+      <CreateOrderSection
+        OnClose={() => toggleModal()}
+        open={open}
+        setOpen={setOpen}
+      />
     </Sidebar>
   );
 }
