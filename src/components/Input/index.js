bb1f158668afe23cfbf6998adee1eaacf413f5fa
emersonteles <emersonteles21@gmail.com>
2021-09-08 19:39:15 -0300

    adding onChange input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable react/prop-types */
 import './styles.css';
 
-export default function Input({ label, name, type }) {
+export default function Input({ label, name, type, onChange }) {
   const inputId = `id_${name}`;
   return (
     <div className="input-container mb-5">
@@ -12,6 +13,7 @@ export default function Input({ label, name, type }) {
           type={type}
           name={name}
           className="input shadow-md"
+          onChange={onChange}
         />
       </label>
     </div>
