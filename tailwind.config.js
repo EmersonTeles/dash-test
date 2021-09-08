d0fdabe074c5c5c3a6584ee194c34515394d6773
emersonteles <emersonteles21@gmail.com>
2021-09-08 15:55:09 -0300

    Add new width style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
 // tailwind.config.js
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    minWidth: {
+      0: '0',
+      '1/4': '25%',
+      half: '50%',
+      '3/4': '75%',
+      full: '100%',
+    },
     extend: {},
   },
   variants: {
