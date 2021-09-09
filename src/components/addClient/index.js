3e9042b57e7c4a3790d2ed9fd54f09340a7db264
emersonteles <emersonteles21@gmail.com>
2021-09-09 17:21:16 -0300

    Turn clients files into components

---
+++ src/components/addClient/index.js
@@ -0,0 +1,101 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { XIcon } from '@heroicons/react/outline';
+import Input from '../Input';
+
+export default function AddClient({ setOpen, open }) {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 overflow-hidden"
+        onClose={setOpen}
+      >
+        <div className="absolute inset-0 overflow-hidden">
+          <Dialog.Overlay className="absolute inset-0" />
+
+          <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
+            <Transition.Child
+              as={Fragment}
+              enter="transform transition ease-in-out duration-500 sm:duration-700"
+              enterFrom="translate-x-full"
+              enterTo="translate-x-0"
+              leave="transform transition ease-in-out duration-500 sm:duration-700"
+              leaveFrom="translate-x-0"
+              leaveTo="translate-x-full"
+            >
+              <div className="w-screen max-w-md">
+                <div className="h-full divide-y divide-gray-200 flex flex-col bg-white shadow-xl">
+                  <div className="min-h-0 flex-1 flex flex-col py-6 overflow-y-scroll">
+                    <div className="px-4 sm:px-6">
+                      <div className="flex items-start justify-between">
+                        <Dialog.Title className="text-lg font-medium text-gray-900">
+                          Cadastrar novos clientes
+                        </Dialog.Title>
+                        <div className="ml-3 h-7 flex items-center">
+                          <button
+                            type="button"
+                            className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                            onClick={() => setOpen(false)}
+                          >
+                            <span className="sr-only">Close panel</span>
+                            <XIcon className="h-6 w-6" aria-hidden="true" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-6 relative flex-1 px-4 sm:px-6">
+                      {/* Replace with your content */}
+                      <Input name="Nome" label="Nome" type="text" />
+                      <p>Pessoa:</p>
+                      <div className="flex items-center justify-between max-w-xs">
+                        <div className="my-4">
+                          <input type="radio" name="fisica" />
+                          <label> Física</label>
+                        </div>
+                        <div>
+                          <input type="radio" name="juridica" />
+                          <label> Jurídica</label>
+                        </div>
+                      </div>
+                      <Input name="Nome" label="CPF/CNPJ" type="text" />
+                      <Input name="Nome" label="Endereço da obra" type="text" />
+                      <Input
+                        name="Nome"
+                        label="Telefone"
+                        type="number"
+                        placeholder="(XX)XXXXXXXX"
+                      />
+                      <Input
+                        name="Nome"
+                        label="Horário de funcionamento da obra"
+                        type="text"
+                      />
+                      {/* /End replace */}
+                    </div>
+                  </div>
+                  <div className="flex-shrink-0 px-4 py-4 flex justify-end">
+                    <button
+                      type="button"
+                      className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      onClick={() => setOpen(false)}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="submit"
+                      className="ml-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
