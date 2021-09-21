58f3b2d1d224cdd3d3829181a560fa452b95fdbd
emersonteles <emersonteles21@gmail.com>
2021-09-21 15:29:37 -0300

    adding login route path

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Pedidos from './pages/pedidos';
 import ClientsPage from './pages/clientsPage';
-import ProductsPage from './pages/productsPage'
+import ProductsPage from './pages/productsPage';
+import Login from './pages/login';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -14,7 +15,7 @@ ReactDOM.render(
       <Route path="/pedidos" component={Pedidos} />
       <Route path="/clientes" component={ClientsPage} />
       <Route path="/produtos" component={ProductsPage} />
-
+      <Route path="/login" component={Login} />
     </Switch>
   </BrowserRouter>,
 
