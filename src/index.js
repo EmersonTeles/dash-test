23081f2fe39ce97ecd706a4b7b102a0996307b35
emersonteles <emersonteles21@gmail.com>
2021-09-28 17:52:00 -0300

    Adding private routes

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/home';
-import Pedidos from './pages/pedidos';
-import ClientsPage from './pages/clientsPage';
-import ProductsPage from './pages/productsPage';
-import Login from './pages/login';
-import Fornecedores from './pages/fornecedores';
 import { AuthContextProvider } from './context/authContext';
+import './index.css';
+import Routes from './routes';
 
 ReactDOM.render(
   <AuthContextProvider>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" component={Login} />
-
-        <Route path="/dashboard" component={Home}>
-
-        </Route>
-        <Route path="/pedidos" component={Pedidos} />
-        <Route path="/clientes" component={ClientsPage} />
-        <Route path="/produtos" component={ProductsPage} />
-        <Route path="/fornecedores" component={Fornecedores} />
-      </Switch>
-    </BrowserRouter>
+    <Routes />
   </AuthContextProvider>,
 
   document.getElementById('root'),
---
+++ src/routes.js
@@ -0,0 +1,43 @@
+/* eslint-disable react/jsx-curly-newline */
+/* eslint-disable no-confusing-arrow */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable react/jsx-props-no-spreading */
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { useContext } from 'react/cjs/react.development';
+import Home from './pages/home';
+import Pedidos from './pages/pedidos';
+import ClientsPage from './pages/clientsPage';
+import ProductsPage from './pages/productsPage';
+import Login from './pages/login';
+import Fornecedores from './pages/fornecedores';
+import { AuthContext } from './context/authContext';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { authenticated } = useContext(AuthContext);
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authenticated ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+};
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+        <Route path="/login" component={Login} />
+        <PrivateRoute path="/dashboard" component={Home} />
+        <PrivateRoute path="/pedidos" component={Pedidos} />
+        <PrivateRoute path="/clientes" component={ClientsPage} />
+        <PrivateRoute path="/produtos" component={ProductsPage} />
+        <PrivateRoute path="/fornecedores" component={Fornecedores} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
